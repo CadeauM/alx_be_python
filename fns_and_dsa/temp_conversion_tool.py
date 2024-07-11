05b6eb92f3fd23656f484deda6993ae59078c6d5
Cadeau Mundoringisa <cadeaudedeu@gmail.com>
2024-07-11 09:02:18 +0200

    add file that coverts temperature to celsius or fahrenheit using conversion factor

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,25 @@
+# this file defines functions to convert temperatures between Celsius and Fahrenheit
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_OFFSET = 32
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - CELSIUS_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + CELSIUS_OFFSET
+    
+def main():
+    temperature = float(input("Enter the temperature: "))
+    unit = input("Enter the unit if it is Celsius/Fahrenheit? C/F: ")
+    if unit == 'C':
+        converted_to_fahrenheit = convert_to_fahrenheit
+        print(f"{temperature}°C is {converted_to_fahrenheit}°F")
+    elif unit == 'F':
+        converted_to_celsius = convert_to_celsius
+        print(f"{temperature}°F is {converted_to_celsius}°C")
+    else:
+        print('Invalid temperature. Please enter a numeric value.')
+    
+    if __name__ == "__main__":
+        main()
